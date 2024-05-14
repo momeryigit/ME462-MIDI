16b5dcf7ace7ca0be5ec28a9e45f13539d53317f
sarpdengizmen <e.dengizmen@gmail.com>
2024-05-14 15:56:11 +0300

    modified entrypoint.sh

--- docker_trial/entrypoint.sh
+++ docker_trial/entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 source /opt/ros/iron/setup.bash
 
-sudo apt-get update
-rosdep update
-rosdep install --from-paths /dev_ws/src --ignore-src -r -y
-sudo rm -rf /var/lib/apt/lists/*
+# sudo apt-get update
+# rosdep update
+# rosdep install --from-paths /dev_ws/src --ignore-src -r -y
+# sudo rm -rf /var/lib/apt/lists/*
 
 echo "Provided command: $@"
 
