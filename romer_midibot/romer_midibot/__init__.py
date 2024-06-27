a32c5ff1a06fd057523b52585dfcf1abce7eb9f4
Omar Habib <omar1farouk@gmail.com>
2024-06-28 01:49:17 +0300

    Setup.py edit

--- romer_midibot/romer_midibot/__init__.py
+++ romer_midibot/romer_midibot/__init__.py
@@ -2,5 +2,7 @@ from .midibot import DifferentialDriveRobot
 from .serial_comm import SerialCommunication
 from .socket_comm import SocketCommunication
 from .sensors import Sensors
+from .default_configs import DefaultConfigs
+from .MPU import MPU
 
-__all__ = ["DifferentialDriveRobot", "SerialCommunication", "SocketCommunication", "Sensors"]
+__all__ = ["DifferentialDriveRobot", "SerialCommunication", "SocketCommunication", "Sensors", "DefaultConfigs", "MPU"]
